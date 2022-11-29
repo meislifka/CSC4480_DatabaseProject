870a6a0cb674a21d7475f88c30e334adf1c9afba
Caroline <cross0108@gmail.com>
2022-11-29 10:23:41 -0500

    Added first table

--- target_tables.sql
+++ target_tables.sql
@@ -1,1 +1,20 @@
+/************************************************************************/
+/*																		*/
+/*	CSC 4480 Final Project				                                */
+/*																		*/
+/*	Database for Target Store Location			                        */
+/*											                            */
+/*	Caroline Ross, Mei Slifka, Shannon Kauber	                        */
+/*												                        */
+/************************************************************************/
 
+DROP TABLE CUSTOMER
+
+CREATE TABLE CUSTOMER(
+    FName       char(20)     NOT NULL,
+    LName       char(30)     NOT NULL,
+    OrderNo     Int          NOT NULL,
+    Address     varchar(100)    NULL,
+    RewardsID   Int             NULL,
+    PRIMARY KEY(Fname, LName, OrderNo)
+);
