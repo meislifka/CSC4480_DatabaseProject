535f9a3172f9d7fc033d2a15cc73f69b42afd5ff
Caroline <cross0108@gmail.com>
2022-11-29 11:36:47 -0500

    Updated credit card no's

--- target_values.sql
+++ target_values.sql
@@ -7,20 +7,20 @@
 /********************************************************************************/
 
 /*****   CUSTOMER DATA   ***********************************************************/
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID, CreditCardNo) VALUES ('Caroline','Ross','102314','111 Lancaster Ave Villanova,PA 19085','12345','7911150699098601');
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID, CreditCardNo) VALUES ('Shannon','Kauber','102345','1034 Countyline Rd Villanova,PA 19085','12346','0100493950178963');
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID, CreditCardNo) VALUES ('Mei','Slifka','304235','205 David Dr Bryn Mawr,PA 19010','12347','4574058294637341');
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID, CreditCardNo) VALUES ('Caroline','Ross','402345','111 Lancaster Ave Villanova,PA 19085','12345','7911150699098601');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID) VALUES ('Caroline','Ross','102314','111 Lancaster Ave Villanova,PA 19085','12345');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID) VALUES ('Shannon','Kauber','102345','1034 Countyline Rd Villanova,PA 19085','12346');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID) VALUES ('Mei','Slifka','304235','205 David Dr Bryn Mawr,PA 19010','12347');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID) VALUES ('Caroline','Ross','402345','111 Lancaster Ave Villanova,PA 19085','12345');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('John','Smith','93012');
 INSERT INTO CUSTOMER(FName, LName, OrderNo, Address) VALUES ('Rachel','Westcott','302452','2031 Main Street Los Angeles, CA 90015');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Shekinah','Yanes','493321');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('James','Patterson','392345');
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID, CreditCardNo) VALUES ('Catherine','Marino','329234','3921 Mill Road Fairfield,NJ 07004','39201','7597053723089211');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, RewardsID) VALUES ('Catherine','Marino','329234','3921 Mill Road Fairfield,NJ 07004','39201');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Shekinah','Yanes','493321');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Katie','Lee','43204352');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Shekinah','Yanes','493321');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Shekinah','Yanes','493321');
-INSERT INTO CUSTOMER(FName, LName, OrderNo, Address, CreditCardNo) VALUES ('Zak','Kermitz','382934','915 Bloomfield Hoboken, NJ 07030','4275940990041965');
+INSERT INTO CUSTOMER(FName, LName, OrderNo, Address) VALUES ('Zak','Kermitz','382934','915 Bloomfield Hoboken, NJ 07030');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Chris','Leonard','932234');
 INSERT INTO CUSTOMER(FName, LName, OrderNo) VALUES ('Benjamin','Yu','8493201');
 
